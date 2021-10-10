3e1cafcd7049919ee0c820c99d236c5adc215dd6
yegorye <yegorye@student.ubc.ca>
2021-10-10 01:29:06 -0700

    A lot of work done. Once the program runs smoothly its time to create some interfaces and abstract classes as code is getting repetitive in places...

--- src/main/model/Exercise.java
+++ src/main/model/Exercise.java
@@ -27,41 +27,77 @@ public class Exercise {
     }
 
     public void printExercise() {
-        int index = 65;
-        exerciseDescription = "[" + exerciseName + "]: " + exerciseDescription;
-        while (exerciseDescription.length() > index) {
-            while (' '  != (exerciseDescription.charAt(index))) {
-                index += 1;
+        int maxLineLength = 65;
+        String exerciseDescription1 = "[" + exerciseName + "]: " + exerciseDescription;
+
+        while (exerciseDescription1.length() > maxLineLength) {
+            while (' ' != (exerciseDescription1.charAt(maxLineLength))) {
+                maxLineLength += 1;
             }
-            String printOut = exerciseDescription.substring(0, index).trim();
+            String printOut = exerciseDescription1.substring(0, maxLineLength).trim();
             System.out.println(printOut);
-            exerciseDescription = exerciseDescription.substring(index);
+            exerciseDescription1 = exerciseDescription1.substring(maxLineLength);
+            maxLineLength = 65;
         }
-        System.out.println(exerciseDescription.trim() + "\n"
-                            + "Number of reps: " + exerciseNumOfReps + "\n"
-                            + "Number of sets: " + exerciseNumOfSets + "\n"
-                            + "Rest time     : " + exerciseRestTime + " seconds\n"
-                            + "Rating        : " + returnDefinedRating());
+
+        System.out.println(exerciseDescription1.trim() + "\n"
+                + "Number of reps: " + exerciseNumOfReps + "\n"
+                + "Number of sets: " + exerciseNumOfSets + "\n"
+                + "Rest time     : " + exerciseRestTime + " seconds\n"
+                + "Rating        : " + returnDefinedRating());
     }
 
     private String returnDefinedRating() {
         String returnedRating;
         switch (exerciseRating) {
             case 0: returnedRating = "F - Atrocious";
-                break;
+                    break;
             case 1: returnedRating = "E - Terrible";
-                break;
+                    break;
             case 2: returnedRating = "D - Ok-ish";
-                break;
+                    break;
             case 3: returnedRating = "C - Decent";
-                break;
+                    break;
             case 4: returnedRating = "B - Great";
-                break;
+                    break;
             case 5: returnedRating = "A - Amazing";
-                break;
-            default: returnedRating = "Unrated";
+                    break;
+            default:
+                returnedRating = "Unrated";
                 break;
         }
         return returnedRating;
+    }
+
+    public String getExerciseName() {
+        return exerciseName;
+    }
+
+    public int getExerciseRating() {
+        return exerciseRating;
+    }
+
+    public void setExerciseName(String name) {
+        exerciseName = name;
+    }
+
+    public void setExerciseDescription(String desc) {
+        exerciseDescription = desc;
+    }
+
+    public void setExerciseNumOfReps(int reps) {
+        exerciseNumOfReps = reps;
+    }
+
+    public void setExerciseNumOfSets(int sets) {
+        exerciseNumOfReps = sets;
+    }
+
+    public void setExerciseRestTime(int restTime) {
+        exerciseNumOfReps = restTime;
+    }
+
+    public void setExerciseRating(int rating) {
+        exerciseNumOfReps = rating;
     }
 }
--- src/main/ui/WorkoutApp.java
+++ src/main/ui/WorkoutApp.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 public class WorkoutApp {
     private Scanner input;
     private List<Exercise> listOfExercises;
+    private List<Exercise> listOfFavoriteExercises;
 
     // EFFECTS: Runs the Workout application
     public WorkoutApp() {
@@ -57,10 +58,8 @@ public class WorkoutApp {
     // EFFECTS:
     private void showMainMenu() {
         System.out.println("\nMain menu, select one of:");
-        System.out.println("\t[1] - view all exercises");
-        System.out.println("\t[2] - view favorite exercises");
-        System.out.println("\t[3] - view routines");
-        System.out.println("\t[4] - view favorite routines");
+        System.out.println("\t[1] - view exercise menu");
+        System.out.println("\t[2] - view routine menu");
         System.out.println("\t[q] - quit");
     }
 
@@ -70,13 +69,9 @@ public class WorkoutApp {
     private void processMenuChoice(String userInput) {
         if (userInput.equals("1")) {
             separatorLine();
-            viewExercises();
+            runExerciseMenu();
         } else if (userInput.equals("2")) {
-            viewFavoriteExercises();
-        } else if (userInput.equals("3")) {
-            viewRoutines();
-        } else if (userInput.equals("4")) {
-            viewFavoriteRoutines();
+            viewRoutinesMenu();
         } else {
             separatorLine();
             System.out.println("\t\t\tSelection not valid");
@@ -87,16 +82,15 @@ public class WorkoutApp {
     // MODIFIES:
     // REQUIRES:
     // EFFECTS:
-    private void viewExercises() {
+    private void runExerciseMenu() {
         boolean goBack = false;
-        String userInput = null;
 
         input = new Scanner(System.in);
         input.useDelimiter("\n");
         while (!goBack) {
             showExerciseMenu();
             System.out.print("choice: ");
-            userInput = input.next();
+            String userInput = input.next();
 
             if (userInput.equals("b")) {
                 goBack = true;
@@ -112,9 +106,11 @@ public class WorkoutApp {
     // EFFECTS:
     private void showExerciseMenu() {
         System.out.println("\nExercise menu, select one of:");
-        System.out.println("\t[1] - add a new exercise");
-        System.out.println("\t[2] - delete an exercise");
-        System.out.println("\t[3] - edit an exercise");
+        System.out.println("\t[1] - view all exercise");
+        System.out.println("\t[2] - view favorite exercises");
+        System.out.println("\t[3] - add a new exercise");
+        System.out.println("\t[4] - delete an exercise");
+        System.out.println("\t[5] - edit an exercise");
         System.out.println("\t[b] - back to main menu");
     }
 
@@ -122,13 +118,27 @@ public class WorkoutApp {
     // REQUIRES:
     // EFFECTS:
     private void viewFavoriteExercises() {
-        //
+        int exerciseCount = 0;
+        for (int i = 0; i < listOfExercises.size(); i++) {
+            Exercise currentExercise = listOfExercises.get(i);
+            if (currentExercise.getExerciseRating() == 5) {
+                separatorLine();
+                exerciseCount += 1;
+                System.out.println("Exercise " + exerciseCount + ":");
+                currentExercise.printExercise();
+            }
+        }
+        if (exerciseCount == 0) {
+            System.out.println("\n\tSeems that you don't have any favorite exercises!");
+            System.out.println("\t(A favorite exercise is either: 'favorite' and/or has a rating of 5)");
+            separatorLine();
+        }
     }
 
     // MODIFIES:
     // REQUIRES:
     // EFFECTS:
-    private void viewRoutines() {
+    private void viewRoutinesMenu() {
         showRoutineMenu();
     }
 
@@ -154,16 +164,36 @@ public class WorkoutApp {
     // REQUIRES:
     // EFFECTS:
     private void processExerciseMenuChoice(String userInput) {
-        if (userInput.equals("1")) {
-            addExercise();
-        } else if (userInput.equals("2")) {
-            deleteExercise();
-        } else if (userInput.equals("3")) {
-            editExercise();
-        } else {
+        switch (userInput) {
+            case "1":
+                viewExercises();
+                break;
+            case "2":
+                viewFavoriteExercises();
+                break;
+            case "3":
+                addExercise();
+                break;
+            case "4":
+                deleteExercise();
+                break;
+            case "5":
+                editExercise();
+                break;
+            default:
+                separatorLine();
+                System.out.println("\t\t\tSelection not valid");
+                separatorLine();
+                break;
+        }
+    }
+
+    private void viewExercises() {
+        System.out.println("\n");
+        for (int i = 1; i <= listOfExercises.size(); i++) {
             separatorLine();
-            System.out.println("\t\t\tSelection not valid");
-            separatorLine();
+            System.out.println("Exercise " + i + ":");
+            listOfExercises.get(i - 1).printExercise();
         }
     }
 
@@ -172,21 +202,205 @@ public class WorkoutApp {
     // EFFECTS: ask user inputs for each field in exercise,
     //          then create new exercise object and add it to the list of exercises
     private void addExercise() {
-        //
+        input = new Scanner(System.in);
+
+        System.out.println("Please input the exercise name");
+        String name = input.nextLine();
+        System.out.println("Please input the exercise description");
+        String description = input.nextLine();
+
+        System.out.println("# reps");
+        int reps = input.nextInt();
+        System.out.println("# sets");
+        int sets = input.nextInt();
+        System.out.println("rest time (seconds)");
+        int restTime = input.nextInt();
+        System.out.println("rating [1-5 or -1]");
+        int rating = input.nextInt();
+        Exercise exercise = new Exercise(name, description, reps, sets, restTime, rating);
+        listOfExercises.add(exercise);
+
+        System.out.println("Exercise successfully added!");
     }
 
     // MODIFIES:
     // REQUIRES:
     // EFFECTS:
     private void deleteExercise() {
-        //
+        String userInput = null;
+        boolean decisionMade = false;
+        input = new Scanner(System.in);
+        System.out.println("Which exercise would you like to delete?");
+        System.out.println("Type [1] to search using the exercise number or [2] to search with its name?");
+        while (!decisionMade) {
+            userInput = input.nextLine();
+            if (userInput.equals("1")) {
+                decisionMade = true;
+                deleteUsingNumber();
+            } else if (userInput.equals("2")) {
+                decisionMade = true;
+                deleteUsingName();
+            }
+        }
     }
+
+    private void deleteUsingNumber() {
+        input = new Scanner(System.in);
+        System.out.println("What's the exercise number?");
+        int userInput = input.nextInt();
+        listOfExercises.remove(userInput - 1);
+    }
+
+    private void deleteUsingName() {
+        input = new Scanner(System.in);
+        boolean foundExercise = false;
+        System.out.println("What's the exercise name?");
+        String userInput = input.nextLine();
+        for (int i = 0; i < listOfExercises.size(); i++) {
+            if (listOfExercises.get(i).getExerciseName().equals(userInput)) {
+                listOfExercises.remove(i);
+                foundExercise = true;
+                break;
+            }
+        }
+        if (foundExercise) {
+            System.out.println("Exercise removed!");
+        } else {
+            System.out.println("Exercise not found!");
+        }
+    }
+
 
     // MODIFIES:
     // REQUIRES:
     // EFFECTS:
     private void editExercise() {
-        //
+        String userInput = null;
+        boolean decisionMade = false;
+        input = new Scanner(System.in);
+        System.out.println("Which exercise would you like to edit?");
+        System.out.println("Type [1] to search using the exercise number or [2] to search with its name?");
+        while (!decisionMade) {
+            userInput = input.nextLine();
+            if (userInput.equals("1")) {
+                decisionMade = true;
+                editUsingNumber();
+            } else if (userInput.equals("2")) {
+                decisionMade = true;
+                editUsingName();
+            }
+        }
+    }
+
+    private void editUsingNumber() {
+    }
+
+    private void editUsingName() {
+        input = new Scanner(System.in);
+        boolean foundExercise = false;
+        System.out.println("What's the exercise name?");
+        String userInput = input.nextLine();
+        for (int i = 0; i < listOfExercises.size(); i++) {
+            Exercise exercise = listOfExercises.get(i);
+            if (exercise.getExerciseName().equals(userInput)) {
+                System.out.println("Exercise found!");
+                foundExercise = true;
+                runEditMenu(exercise);
+            }
+        }
+        if (foundExercise)
+            System.out.println("Exercise not found!");
+        }
+    }
+
+    private void runEditMenu(Exercise exercise) {
+        boolean goBack = false;
+        input = new Scanner(System.in);
+        input.useDelimiter("\n");
+        while (!goBack) {
+            showEditMenu();
+            System.out.print("choice: ");
+            String userInput = input.next();
+
+            if (userInput.equals("b")) {
+                goBack = true;
+                separatorLine();
+            } else {
+                processEditMenuChoice(userInput, exercise);
+            }
+        }
+    }
+
+    private void showEditMenu() {
+        System.out.println("Select all information which you would like to change");
+        System.out.println("\t[1] - to change name");
+        System.out.println("\t[2] - to change description");
+        System.out.println("\t[3] - to change number of reps");
+        System.out.println("\t[4] - to change number of sets");
+        System.out.println("\t[5] - to change rest time");
+        System.out.println("\t[6] - to change rating");
+        System.out.println("\t[b] - to go back to Exercise menu");
+        System.out.println("e.g. if you want to change the \"Name\" and \"Rest time\" -> type 16");
+    }
+
+    private void processEditMenuChoice(String userInput, Exercise exercise) {
+        for (int i = 0; i < userInput.length(); i++) {
+            if (userInput.charAt(i) == '1') {
+                editExerciseName(exercise);
+            } else if (userInput.charAt(i) == '2') {
+                editExerciseDescription(exercise);
+            } else if (userInput.charAt(i) == '3') {
+                editExerciseNoR(exercise);
+            } else if (userInput.charAt(i) == '4') {
+                editExerciseNoS(exercise);
+            } else if (userInput.charAt(i) == '5') {
+                editExerciseRestTime(exercise);
+            } else if (userInput.charAt(i) == '6') {
+                editExerciseRating(exercise);
+            }
+        }
+    }
+
+    private void editExerciseName(Exercise exercise) {
+        System.out.println("Please input the new name: ");
+        input = new Scanner(System.in);
+        String userInput = input.nextLine();
+        exercise.setExerciseName(userInput);
+    }
+
+    private void editExerciseDescription(Exercise exercise) {
+        System.out.println("Please input the new descriptions: ");
+        input = new Scanner(System.in);
+        String userInput = input.nextLine();
+        exercise.setExerciseDescription(userInput);
+    }
+
+    private void editExerciseNoR(Exercise exercise) {
+        System.out.println("Please input the new number of reps: ");
+        input = new Scanner(System.in);
+        int userInput = input.nextInt();
+        exercise.setExerciseNumOfReps(userInput);
+    }
+
+    private void editExerciseNoS(Exercise exercise) {
+        System.out.println("Please input the new number of sets: ");
+        input = new Scanner(System.in);
+        int userInput = input.nextInt();
+        exercise.setExerciseNumOfSets(userInput);
+    }
+
+    private void editExerciseRestTime(Exercise exercise) {
+        System.out.println("Please input the new rest time: ");
+        input = new Scanner(System.in);
+        int userInput = input.nextInt();
+        exercise.setExerciseRestTime(userInput);
+    }
+
+    private void editExerciseRating(Exercise exercise) {
+        System.out.println("Please input the new rating: ");
+        input = new Scanner(System.in);
+        int userInput = input.nextInt();
+        exercise.setExerciseRating(userInput);
     }
 
     private void separatorLine() {
